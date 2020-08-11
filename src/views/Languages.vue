35da8fe8bbe97db964119e6b8eb6828ed9368f94
yuhkiikd <yuhki.ikd@gmail.com>
2020-08-11 17:50:58 +0900

    言語ページのスタイル設定

--- src/views/Languages.vue
+++ src/views/Languages.vue
@@ -2,21 +2,21 @@
   <div class="languages">
     <Header>Languages</Header>
     <b-container class="bv-example-row">
-      <b-row class="justify-content-md-center mb-2">
-        <b-col cols="sm-3" class="label-title-html py-4 text-white">Html</b-col>
-        <b-col cols="sm-9" class="label-content-html py-4 bg-light">説明</b-col>
+      <b-row class="justify-content-md-center mb-2 text-white">
+        <b-col cols="sm-3" class="label-title-html py-4">Html</b-col>
+        <b-col cols="sm-9" class="label-content-html label-ribbon py-4">説明</b-col>
       </b-row>
-      <b-row class="justify-content-md-center mb-2">
+      <b-row class="justify-content-md-center mb-2 text-white">
         <b-col cols="sm-3" class="label-title-css py-4 text-white">CSS</b-col>
-        <b-col cols="sm-9" class="label-content-css py-4 bg-light">説明</b-col>
+        <b-col cols="sm-9" class="label-content-css label-ribbon py-4">説明</b-col>
       </b-row>
       <b-row class="justify-content-md-center mb-2">
         <b-col cols="sm-3" class="label-title-js py-4">JavaScript</b-col>
-        <b-col cols="sm-9" class="label-content-js py-4 bg-light">説明</b-col>
+        <b-col cols="sm-9" class="label-content-js label-ribbon py-4">説明</b-col>
       </b-row>
-      <b-row class="justify-content-md-center">
+      <b-row class="justify-content-md-center text-white">
         <b-col cols="sm-3" class="label-title-ruby py-4 text-white">Ruby</b-col>
-        <b-col cols="sm-9" class="label-content-ruby py-4 bg-light">説明</b-col>
+        <b-col cols="sm-9" class="label-content-ruby label-ribbon py-4">説明</b-col>
       </b-row>
     </b-container>
   </div>
