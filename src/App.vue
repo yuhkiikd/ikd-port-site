f0674090432d2e824bae7ab58b54486a447d9955
yuhkiikd <yuhki.ikd@gmail.com>
2020-08-19 13:28:51 +0900

    vue-loadingを修正

--- src/App.vue
+++ src/App.vue
@@ -78,7 +78,7 @@ export default {
   mounted() {
     setTimeout(() => {
       this.loading = false;
-    }, 1000);
+    }, 2000);
   },
 };
 </script>
--- src/components/Loading.vue
+++ src/components/Loading.vue
@@ -20,7 +20,6 @@ export default {
   components: {
     VueLoading,
   },
-
 }
 </script>
 
