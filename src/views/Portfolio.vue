843b88b14dcad50999b08a17b14cc17a6521935c
yuhkiikd <yuhki.ikd@gmail.com>
2020-08-19 11:29:11 +0900

    portfolioページ更新

--- src/views/Portfolio.vue
+++ src/views/Portfolio.vue
@@ -2,9 +2,9 @@
   <div class="portfolio">
     <Header>Portfolio</Header>
     <b-container class="bv-example-row container-fluid">
-      <b-row class="justify-content-md-center mb-md-5 mb-sm-3">
+      <b-row class="justify-content-md-center mb-5">
         <b-col cols="sm-3" class="label-title py-3 d-flex justify-content-center align-items-center"><a href="http://52.197.221.44/users/sign_in" target="_blank"><img src="../assets/timecard.jpg" width="100%"></a></b-col>
-        <b-col cols="sm-9" class="label-content label-ribbon text-left py-2">
+        <b-col cols="sm-9" class="label-content label-ribbon text-left py-md-2 py-sm-0">
           <p class="mb-0">
             <ul class="mb-0 list-group list-group-flush">
               <li class="list-group-item">勤怠管理アプリ（Ruby on Rails、AWS）<br>【<a href="http://52.197.221.44/users/sign_in" target="_blank">リンク</a>】【<a href="https://github.com/yuhkiikd/timecard" target="_blank">github</a>】</li>
@@ -16,11 +16,11 @@
         </b-col>
       </b-row>
       <b-row class="justify-content-md-center mb-3">
-        <b-col cols="sm-3" class="label-title py-3 d-flex justify-content-center align-items-center"><a href="http://52.197.221.44/users/sign_in" target="_blank"><img src="../assets/timecard.jpg" width="100%"></a></b-col>
-        <b-col cols="sm-9" class="label-content label-ribbon text-left py-2">
+        <b-col cols="sm-3" class="label-title py-3 d-flex justify-content-center align-items-center"><a href="https://ikd-port-site.herokuapp.com/"><img src="../assets/portsite.jpg" width="100%"></a></b-col>
+        <b-col cols="sm-9" class="label-content label-ribbon text-left py-md-2 py-sm-0">
           <p class="mb-0">
             <ul class="mb-0 list-group list-group-flush" height="100%">
-              <li class="list-group-item">ikeda-port-site（Vue.cli、heroku）<br>【<a href="https://ikd-port-site.herokuapp.com/" target="_blank">リンク</a>】【<a href="https://github.com/yuhkiikd/ikd-port-site" target="_blank">github</a>】</li>
+              <li class="list-group-item">ikeda-port-site（Vue.cli、heroku）<br>【<a href="https://ikd-port-site.herokuapp.com/">リンク</a>】【<a href="https://github.com/yuhkiikd/ikd-port-site" target="_blank">github</a>】</li>
               <li class="list-group-item">制作期間：約1週間</li>
               <li class="list-group-item">本サイトです。Qiitaの記事を参考に、Vue.cliの試用も兼ねて制作しました。<br>サクサク動くのが素晴らしい。</li>
             </ul>
