794e1ea994f4886e71a765a51316df2383d1c6f7
yuhkiikd <yuhki.ikd@gmail.com>
2020-08-19 21:45:21 +0900

    Home画像とh1余白変更、

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
       <span>W</span>orld!
     </h1>
         <vue-loading type="spin" color="#333" :size="{ width: '50px', height: '50px' }"></vue-loading>
-    <div style="height:auto"><img src="https://source.unsplash.com/640x360/?smile" alt="smile"></div>
+    <div style="height:auto"><img src="https://source.unsplash.com/640x360/?coffee" alt="smile"></div>
     <p>
       初めまして、Ikedaです。<br>
       ここはIkedaのポートフォリオサイトです。
